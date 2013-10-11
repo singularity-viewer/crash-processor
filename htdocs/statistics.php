e19d1111ad12ce6845608744278e257d036cf434
Latif Khalifa <latifer@streamgrid.net>
2013-10-12 01:01:30 +0200

    Layout tabs for stats page

--- htdocs/statistics.php
+++ htdocs/statistics.php
@@ -3,11 +3,50 @@
 define("SITE_ROOT", realpath(dirname(__file__)));
 require_once SITE_ROOT . "/lib/init.php";
 $S->requireUser();
+$filter = new SearchFilter();
 
 Layout::header();
+$filter->render();
 ?>
+<script>
+    $(function() {
+        $( "#tabs" ).tabs({ active: 1 });
+        // $("div.ui-tabs-panel").css('padding','0px');
+    });
+</script>
+<br/>
+<div id="tabs">
+    <ul>
+        <li><a href="#tab-1">Top Crashers</a></li>
+        <li><a href="#tab-2">Graphics Cards</a></li>
+        <li><a href="#tab-3">Operating Systems</a></li>
+        <li><a href="#tab-4">Regions</a></li>
+    </ul>
 
-<strong>Not implemented</strong>
+
+    <!-- top crashers tab -->
+    <div id="tab-1">
+    </div>
+    <!-- /top crashers tab -->
+
+
+    <!-- gpu tab -->
+    <div id="tab-2">
+    </div>
+    <!-- /gpu tab -->
+
+
+    <!-- os tab -->
+    <div id="tab-3">
+    </div>
+    <!-- /os tab -->
+
+
+    <!-- regions tab -->
+    <div id="tab-4">
+    </div>
+    <!-- /regions tab -->
+</div>
 
 <?php
 Layout::footer();
